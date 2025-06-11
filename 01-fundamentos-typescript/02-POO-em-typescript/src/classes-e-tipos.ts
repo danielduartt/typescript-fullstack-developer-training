c3df324a926757f962ba8ca68ac7a30c1eb90d0e
danielduartt <dev.danie.duarte@gmail.com>
2025-06-11 11:31:22 -0300

    fix: add arq sobre classes em typescript

---
+++ 01-fundamentos-typescript/02-POO-em-typescript/src/classes-e-tipos.ts
@@ -0,0 +1,38 @@
+class User {
+    name: string = "Daniel"; 
+    age: number = 32; 
+
+    constructor(name: string, age: number){
+        this.name = name; 
+        this.age = age; 
+    }
+
+    showName = () => {
+        console.log(this.name); }
+}
+const user: User = new User("Daniel", 22); 
+//user.showName(); 
+
+const otherUser = new User("Maria", 32); 
+//otherUser.showName(); 
+
+
+// Dio Bank 
+
+class Account {
+    name: string | undefined; 
+    accountNumber: number; 
+
+    constructor(name: string, accountNumber: number){
+        this.name = name; 
+        this.accountNumber = accountNumber;
+    }
+
+    deposit = () => {
+        console.log("Você depositou..."); 
+    }
+
+    withdraw = () => {
+        console.log("Você sacou..."); 
+    }
+}
