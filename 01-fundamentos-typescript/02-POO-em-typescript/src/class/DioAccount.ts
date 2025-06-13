528a9608525981af1a3083cc1eb22a4fa8d4dbba
danielduartt <dev.danie.duarte@gmail.com>
2025-06-13 09:31:58 -0300

    fix: add visibilidade as classes

--- 01-fundamentos-typescript/02-POO-em-typescript/src/class/DioAccount.ts
+++ 01-fundamentos-typescript/02-POO-em-typescript/src/class/DioAccount.ts
@@ -2,13 +2,16 @@ export abstract class DioAccount {
     private name: string; 
     accountNumber: number; 
     balance: number = 0; 
+    private status: boolean  = true; 
 
     constructor(name: string, accountNumber: number){
         this.name = name; 
         this.accountNumber = accountNumber;
     }
     deposit = () : void => {
-        console.log("Você depositou..."); 
+        if(this.validadeStatus()){
+            console.log("Você depositou..."); 
+        }
     }
     withdraw = () : void => {
         console.log("Você sacou..."); 
@@ -21,5 +24,14 @@ export abstract class DioAccount {
     }
     setName = (name: string) : void => {
         this.name = name; 
+    }
+
+    // private pois acontece apenas de forma interna a classe e o obj instanciado
+    private validadeStatus = (): boolean=> {
+        if(this.status){
+            return this.status; 
+        }else{
+            throw new Error("Conta inválida"); 
+        }
     }
 }
